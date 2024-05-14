206782db3a6cbedc5c81251c6b798c01f0a75542
Max <maxence.tourniayre@gmail.com>
2024-05-14 19:18:27 +0200

    fix: add missing logger provider

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ repositories {
 dependencies {
     implementation(kotlin("reflect"))
     implementation("org.jetbrains.kotlinx:kotlin-statistics-jvm:0.2.1")
+    implementation("org.slf4j:slf4j-simple:2.0.13")
+    implementation("org.slf4j:slf4j-api:2.0.13")
     testImplementation(kotlin("test"))
 }
 
