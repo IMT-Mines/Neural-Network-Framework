ae6b5effb7c06882782b57ef59fb6c4abaf0480d
Max <maxence.tourniayre@gmail.com>
2024-05-22 11:47:55 +0200

    feat: add new environment

--- src/main/kotlin/train/deepReinforcement/Environment.kt
+++ src/main/kotlin/train/deepReinforcement/Environment.kt
@@ -1,85 +1,65 @@
-//class State(val position: Pair<Double, Double>)
-//
-//enum class Action {
-//    FORWARD, BACKWARD, TURN_LEFT, TURN_RIGHT
-//}
-//
-//class Environment(private val mapSize: Int) {
-//
-//    private var map: Array<Array<Int>> = Array(mapSize) { Array(mapSize) { 0 } }
-//    private var agentPosition: Pair<Int, Int> = Pair(mapSize - 1, 0)
-//
-//
-//    fun reset(): State {
-//        map = Array(mapSize) { Array(mapSize) { 0 } }
-//        agentPosition = Pair(mapSize - 1, 0)
-//        map[agentPosition.first][agentPosition.second] = 1
-//        return getState()
-//    }
-//
-//    private fun getState(): State {
-//        return S
-//    }
-//
-//    fun action(action: Int) {
-//        val currentX = agentPosition.first
-//        val currentY = agentPosition.second
-//        map[currentX][currentY] = 0
-//
-//        when (action) {
-//            0 -> {
-//                if (currentX > 0) {
-//                    agentPosition = Pair(currentX - 1, currentY)
-//                }
-//            }
-//
-//            1 -> {
-//                if (currentX < mapSize - 1) {
-//                    agentPosition = Pair(currentX + 1, currentY)
-//                }
-//            }
-//
-//            2 -> {
-//                if (currentY > 0) {
-//                    agentPosition = Pair(currentX, currentY - 1)
-//                }
-//            }
-//
-//            3 -> {
-//                if (currentY < mapSize - 1) {
-//                    agentPosition = Pair(currentX, currentY + 1)
-//                }
-//            }
-//        }
-//        map[agentPosition.first][agentPosition.second] = 1
-//    }
-//
-//    fun getReward(): Double {
-//        val currentX = agentPosition.first
-//        val currentY = agentPosition.second
-//
-//        return if (currentX == 2 && currentY == 2) {
-//            1.0
-//        } else if (currentX == 1 && currentY == 1) {
-//            -1.0
-//        } else {
-//            0.0
-//        }
-//    }
-//
-//    fun isTerminal(): Boolean {
-//        val currentX = agentPosition.first
-//        val currentY = agentPosition.second
-//        return currentX == 2 && currentY == 2 || currentX == 1 && currentY == 1
-//    }
-//
-//    fun step(action: Action): Pair<State, Double> {
-//        action(action.ordinal)
-//        val reward = getReward()
-//        return Pair(getState(), reward)
-//    }
-//
-//    override fun toString(): String {
-//        return map.joinToString("\n") { it.joinToString(" ") }
-//    }
-//}
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+data class State(val states: DoubleArray)
+
+enum class Action {
+    FORWARD, BACKWARD, TURN_LEFT, TURN_RIGHT
+}
+
+interface Environment {
+    fun reset(): State
+    fun step(action: Action): Pair<State, Double>
+    fun isDone(): Boolean
+}
+
+class FoodGameEnvironment(
+    private val mapSize: Int = 5, // 5x5 map | 1 = player, -1 = food
+    private var state: State? = null,
+    private val foodPosition: Pair<Int, Int> = Pair(3, 3),
+    private var playerPosition: Pair<Int, Int> = Pair(0, 0)
+) : Environment {
+
+    override fun reset(): State {
+        state = state ?: State(DoubleArray(mapSize * mapSize) { 0.0 })
+        state!!.states[playerPosition.first * mapSize + playerPosition.second] = 1.0
+        state!!.states[foodPosition.first * mapSize + foodPosition.second] = -1.0
+        return state!!
+    }
+
+    override fun step(action: Action): Pair<State, Double> {
+        val ordinal = 2;
+        val (x, y) = playerPosition
+        val distance = calculateDistance()
+        when (action) {
+            Action.FORWARD -> {
+                if (x < mapSize - 1) playerPosition = Pair(x + 1, y)
+            }
+            Action.BACKWARD -> {
+                if (x > 0) playerPosition = Pair(x - 1, y)
+            }
+            Action.TURN_LEFT -> {
+                if (y > 0) playerPosition = Pair(x, y - 1)
+            }
+            Action.TURN_RIGHT -> {
+                if (y < mapSize - 1) playerPosition = Pair(x, y + 1)
+            }
+        }
+
+        var reward = -1.0
+        val newDistance = calculateDistance()
+        reward += (newDistance - distance) * 2
+        return Pair(State(DoubleArray(mapSize * mapSize) { 0.0 }), reward)
+    }
+
+    override fun isDone(): Boolean {
+        return playerPosition.first == foodPosition.first && playerPosition.second == foodPosition.second
+    }
+
+    private fun calculateDistance(): Double {
+        val (x1, y1) = playerPosition
+        val (x2, y2) = foodPosition
+        return sqrt((x2 - x1).toDouble().pow(2) + (y2 - y1).toDouble().pow(2))
+    }
+
+}
