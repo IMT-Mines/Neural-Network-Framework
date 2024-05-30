3adf8a9d49e62a7db676e4cf6151d31bedd9942e
Max <maxence.tourniayre@gmail.com>
2024-05-30 09:35:29 +0200

    fix: jitpack publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    `maven-publish`
     kotlin("jvm") version "1.9.23"
+    `maven-publish`
 }
 
 group = "org.neuralnetwork"
@@ -13,6 +13,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("reflect"))
+    implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlin-statistics-jvm:0.2.1")
     implementation("org.slf4j:slf4j-simple:2.0.13")
     implementation("org.slf4j:slf4j-api:2.0.13")
@@ -22,15 +23,13 @@ kotlin {
     jvmToolchain(21)
 }
 
-afterEvaluate {
-    publishing {
-        publications {
-            register("release", MavenPublication::class) {
-                from(components["java"])
-                groupId = project.group.toString()
-                artifactId = project.name
-                version = project.version.toString()
-            }
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
         }
     }
 }
