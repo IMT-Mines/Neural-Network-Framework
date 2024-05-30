9b725a65152311d6881780d5ac2c9d4a6fc4cbd6
Max <maxence.tourniayre@gmail.com>
2024-05-30 09:10:16 +0200

    fix: jitpack publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    java
     `maven-publish`
     kotlin("jvm") version "1.9.23"
 }
@@ -14,7 +13,6 @@ repositories {
 
 dependencies {
     implementation(kotlin("reflect"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.0")
     implementation("org.jetbrains.kotlinx:kotlin-statistics-jvm:0.2.1")
     implementation("org.slf4j:slf4j-simple:2.0.13")
     implementation("org.slf4j:slf4j-api:2.0.13")
@@ -24,19 +22,15 @@ kotlin {
     jvmToolchain(21)
 }
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "21"
-}
-
-
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
+afterEvaluate {
+    publishing {
+        publications {
+            register("release", MavenPublication::class) {
+                from(components["java"])
+                groupId = project.group.toString()
+                artifactId = project.name
+                version = project.version.toString()
+            }
         }
     }
 }
